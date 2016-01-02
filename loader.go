3e6a0308bd41edc0d4b94a8ceb35c04d7f726819
Florian Hines <syn@ronin.io>
2016-01-02 16:37:10 -0600

    Add owner concept and support github.com/unrolled/render

--- loader.go
+++ loader.go
@@ -9,6 +9,20 @@
 //  res := dbtmpl.RenderTemplateFile("templates/tiny.pongo", pongo2.Context{"name": "florian"})
 //
 // It also has some additional methods to let you manage your templates.
+// You can also use it with vanilla go templates via something like github.com/unrolled/render by
+// using the RethinkLoader ala:
+//
+//  opts := pongo2rethink.Opts{
+//   TableName: "templates",
+//   Prefix:    "randocustomer",
+//   Session:   s.rethink,
+//  }
+//  l := pongo2rethink.NewRethinkLoader(&opts))
+//  render.New(render.Options{
+//   Asset: l.GetTemplateBytes,
+//   AssetNames: l.GetTemplateNames,
+//  })
+//
 package pongo2rethink
 
 import (
@@ -18,6 +32,7 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"strings"
 
 	rethink "github.com/dancannon/gorethink"
 	"github.com/flosch/pongo2"
@@ -27,6 +42,7 @@ type Opts struct {
 	TableName string           //Table to use
 	Prefix    string           //optional prefix to apply to all paths
 	Session   *rethink.Session //The rethink session that should be used
+	Owner     string           //What if anything to put in an "owner" field, empty string is omitted
 }
 
 type RethinkTemplateLoader struct {
@@ -42,17 +58,17 @@ func NewRethinkLoader(opts *Opts) *RethinkTemplateLoader {
 }
 
 type Template struct {
-	Name string
-	Data string
+	Name  string
+	Data  string
+	Owner string `gorethink:",omitempty"`
 }
 
 func (t *RethinkTemplateLoader) fetchTemplate(path string) (*Template, error) {
 	var template Template
-	rpath := t.r.Prefix + "/" + path
+	rpath := t.r.Owner + t.r.Prefix + "/" + path
 	log.Println("fetching:", rpath)
 	cursor, err := rethink.Table(t.r.TableName).Get(rpath).Run(t.r.Session)
 	if err != nil {
-
 		return &template, err
 	}
 	err = cursor.One(&template)
@@ -84,7 +100,8 @@ func (t *RethinkTemplateLoader) LoadTemplate(template Template) error {
 // LoadTemplateFromFile reads a given file and loads it into rethink
 func (t *RethinkTemplateLoader) LoadTemplateFromFile(path string) (err error) {
 	var template Template
-	template.Name = t.r.Prefix + "/" + path
+	template.Owner = t.r.Owner
+	template.Name = t.r.Owner + t.r.Prefix + "/" + path
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return err
@@ -108,19 +125,20 @@ func (t *RethinkTemplateLoader) LoadTemplatesFromDir(dir, pattern string) error 
 		return err
 	}
 	var template Template
+	template.Owner = t.r.Owner
 	for _, tfile := range tfiles {
-		template.Name = t.r.Prefix + "/" + tfile
+		template.Name = t.r.Owner + t.r.Prefix + "/" + tfile
 		data, err := ioutil.ReadFile(tfile)
 		if err != nil {
 			return err
 		}
 		template.Data = string(data)
 		// TODO: batch insert ?
-		result, err := rethink.Table(t.r.TableName).Insert(template).RunWrite(t.r.Session)
+		result, err := rethink.Table(t.r.TableName).Insert(template, rethink.InsertOpts{Conflict: "replace"}).RunWrite(t.r.Session)
 		if err != nil {
 			return err
 		}
-		if result.Inserted == 0 && result.Replaced == 0 {
+		if result.Inserted == 0 && result.Replaced == 0 && result.Unchanged == 0 {
 			return fmt.Errorf("Encountered weird state on template insert: %s", fmt.Sprintf("%+v", result))
 		}
 	}
@@ -138,6 +156,33 @@ func (t *RethinkTemplateLoader) GetTemplateBytes(path string) ([]byte, error) {
 func (t *RethinkTemplateLoader) GetTemplateString(path string) (string, error) {
 	template, err := t.fetchTemplate(path)
 	return template.Data, err
+}
+
+// GetTemplateNames retrieves a list of all current template names from rethink
+// It goes against standards in that it ONLY PRINTS ERRORS if it encounters any,
+// to satisfy github.com/unrolled/render AssetNames()
+func (t *RethinkTemplateLoader) GetTemplateNames() []string {
+	var err error
+	var cursor *rethink.Cursor
+	if t.r.Owner == "" {
+		cursor, err = rethink.Table(t.r.TableName).Pluck("Name").Run(t.r.Session)
+	} else {
+		cursor, err = rethink.Table(t.r.TableName).Filter(map[string]string{"Owner": "randocustomer"}).Pluck("Name").Run(t.r.Session)
+	}
+	if err != nil {
+		log.Println("Error trying to list template names:", err)
+	}
+	defer cursor.Close()
+	var resp map[string]string
+	pfx := t.r.Owner + t.r.Prefix + "/"
+	var names []string
+	for cursor.Next(&resp) {
+		names = append(names, strings.TrimPrefix(resp["Name"], pfx))
+	}
+	if cursor.Err() != nil {
+		log.Println(cursor.Err)
+	}
+	return names
 }
 
 // Abs returns the absolute path and is used by pongo2
